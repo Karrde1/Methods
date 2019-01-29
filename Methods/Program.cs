a06d7aeed39e9b520312841b50391800144b980b
Gregory Gauthier <ggauthier@mail.usf.edu>
2019-01-29 13:38:58 -0500

    An Overview of Basic Method Use

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿/* Author: Gregory Gauthier
+ * Date: 1/25/2019
+ * Comments: This Program Will Demonstrate Methods
+ */
+
+using System;
 
 namespace Methods
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Greetings User! \n Please Enter your Name!");
+            Console.ReadKey(true);
+            Console.WriteLine("Please Enter Your Name");
+
+            try
+            {
+                string UserInput = Console.ReadLine();
+            }
+
+            catch
+            {
+                Console.WriteLine("Please Enter a Valid Name and Try Again");
+                Console.WriteLine("Press Any Key To Exit The Program");
+                Console.ReadKey(true);
+            }
+
+        }
+
         }
     }
+
+private void UserReturn()
+{
+    Console.WriteLine("Hello" + UserInput.ToString());
+
 }
